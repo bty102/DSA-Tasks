54868ce5f1bea98233d693f9525ded4732f5011d
BTY <phuocpham044@gmail.com>
2024-02-23 02:33:46 +0700

    update: bai1_DQ.cpp

--- baiTap1/bai1_DQ.cpp
+++ baiTap1/bai1_DQ.cpp
@@ -1,52 +1,99 @@
 #include <iostream>
 
-// 1. Tim max cua day A
+/*
+1. Tim gia tri lon nhat cua day A.
+int A[]: mang can tim GTLN.
+int n: kich thuoc cua mang.
+*/
 int MAX(int A[], int n) {
-    if(n == 1) return A[0];
-    return (A[n-1] > MAX(A, n-1))? A[n-1] : MAX(A, n-1);
+    if(n == 1) return A[1];
+    int m = MAX(A, n-1);
+    return ((A[n] > m)? A[n] : m);
 }
 
-// 2. Dao nguoc day A
-void DaoNguoc(int A[], int n) {
-    if(n == 1) return;
-    int tmp = A[0];
-    for(int i = 0; i <= n-2; i++) {
-        A[i] = A[i+1];
+/*
+2. Dao nguoc day A.
+int A[]: day muon dao nguoc.
+int start_pos: chi so phan tu dau.
+int end_pos: chi so phan tu cuoi.
+*/
+void DaoNguoc(int A[], int start_pos, int end_pos) {
+    if(start_pos < end_pos) {
+        std::swap(A[start_pos], A[end_pos]);
+        DaoNguoc(A, start_pos+1, end_pos-1);    
     }
-    A[n-1] = tmp;
-    DaoNguoc(A, n-1);
 }
 
-// 3. Kiem tra day A co doi xung hay khong
+/*
+3. Kiem tra day A co doi xung hay khong.
+int A[]: day muon kiem tra doi xung.
+int start_pos: chi so phan tu dau.
+int end_pos: chi so phan tu cuoi.
+*/
 bool DoiXung(int A[], int start_pos, int end_pos) {
-    if(start_pos == end_pos or start_pos > end_pos) return true;
-    if(A[start_pos] != A[end_pos]) return false;
-    return DoiXung(A, start_pos+1, end_pos-1);
+    if(start_pos >= end_pos) return true;
+    return ((A[start_pos] != A[end_pos])? false : DoiXung(A, start_pos+1, end_pos-1));
 }
 
-// 4. Dem so cap nghich dao cua A
+/*
+4. Dem so cap nghich dao trong A.
+int A[]: day can dem so cap nghich dao.
+int n: kich thuoc cua day.
+*/
 int SoCapND(int A[], int n) {
     if(n == 1) return 0;
-    int SND = 0;
-    for(int i = n-2; i >= 0; i--) {
-        if(A[n-1] < A[i]) SND++;
+    int d = 0;
+    for(int i = n-1; i >= 1; i--) {
+        if(A[n] < A[i]) d++;
     }
-    return SND + SoCapND(A, n-1);
+    return d + SoCapND(A, n-1);
+}
+
+/*
+5. Dem so cap trong A ma co tong bang x cho truoc.
+int A[]: day can dem.
+int n: kich thuoc day.
+int x: so cho truoc.
+*/
+int SoCap(int A[], int n, int x) {
+    if(n == 1) return 0;
+    int d = 0;
+    for(int i = n-1; i >= 1; i--) {
+        if(A[n] + A[i] == x) d++;
+    }
+    return d + SoCap(A, n-1, x);
+}
+
+/*
+6.
+*/
+void InDay(int A[], int start_pos, int end_pos) {
+    if(start_pos == end_pos) {std::cout << 0; return;}
+    int d = 0;
+    for(int i = start_pos+1; i <= end_pos; i++) {
+        if(A[i] <= A[start_pos]) d++;
+    }
+    std::cout << d << " ";
+    InDay(A, start_pos+1, end_pos);
 }
 int main() {
     int n;
     std::cin >> n;
-    int A[n];
-    for(int i = 0; i <= n-1; i++) {
+    int A[n+1];
+    for(int i = 1; i <= n; i++) {
         std::cin >> A[i];
     }
-    // std::cout << "Max = " << MAX(A, n) << "\n";
-    // DaoNguoc(A, n);
+    // std::cout << "Gia tri lon nhat: " << MAX(A, n);
+    // DaoNguoc(A, 1, n);
     // std::cout << "Sau khi dao nguoc: ";
-    // for(int i = 0; i <= n-1; i++) {
+    // for(int i = 1; i <= n; i++) {
     //     std::cout << A[i] << " ";
     // }
-    // std::cout << (DoiXung(A, 0, n-1)? "La doi xung\n" : "La khong doi xung\n");
-    std::cout << "So cap nghich dao: " << SoCapND(A, n);
+    // std::cout << (DoiXung(A, 1, n)? "La doi xung" : "La khong doi xung");
+    // std::cout << "So cap nghich dao: " << SoCapND(A, n);
+    // int x;
+    // std::cin >> x;
+    // std::cout << "So cap co tong bang " << x << " la: " << SoCap(A, n, x);
+    InDay(A, 1, n);
     return 0;
 }
